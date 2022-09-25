4b7e38443a94cc2e4db1d3459a95aaa35c205ca3
Bobtroopo <53283372+Paladise@users.noreply.github.com>
2022-09-25 13:32:37 -0400

    Update logo

--- static/js/file_upload.js
+++
@@ -1,5 +0,0 @@
-const dropZone = document.getElementById("dropZone");
-const dropZoneInput = document.getElementById("dropZoneInput");
-dropZone.addEventListener("click", function(event) {
-    dropZoneInput.click();
-});

6c4f818487977b4b6461eb5dec0c9976167c61cd
Bobtroopo <53283372+Paladise@users.noreply.github.com>
2022-10-03 20:32:28 -0400

    Randomise order of our names

--- static/js/home.js
+++ static/js/home.js
@@ -3,3 +3,10 @@ const dropZoneInput = document.getElementById("dropZoneInput");
 dropZone.addEventListener("click", function(event) {
     dropZoneInput.click();
 });
+
+const developers = document.getElementById("developers");
+if(Math.round(Math.random()) == 0) {
+    developers.innerHTML = "Developed by Sean Su & Andrei Basto (2023)"
+}else{
+    developers.innerHTML = "Developed by Andrei Basto & Sean Su (2023)"
+}

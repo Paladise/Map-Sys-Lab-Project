e62c2a2d6c777d3e519b7c0785d3f1830b76e3c8
Bobtroopo <53283372+Paladise@users.noreply.github.com>
2022-10-19 20:46:35 -0400

    Update initial findings

--- initial_tesseract_try.py
+++ initial_tesseract_try.py
@@ -6,6 +6,7 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 
 # Grayscale, Gaussian blur, Otsu's threshold
 image = cv.imread("images/floor1.jpg")
+image2 = image.copy()
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 blur = cv.GaussianBlur(gray, (3,3), 0)
 thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
@@ -19,7 +20,7 @@ invert = 255 - opening
 # Add boxes using pytesseract
 
 pil_img = Image.open("images/floor1.jpg")
-pix = pil_img.load()
+pil_img2 = pil_img.copy()
 h, w, c = image.shape
 
 boxes = pytesseract.image_to_boxes(invert)
@@ -32,11 +33,7 @@ for b in boxes.splitlines():
     y2 = h - int(b[4])
 
     if abs(x1 - x2) * abs(y1 - y2) < 1200 and abs(x1 - x2) > 10 and abs(y1 - y2) > 10:
-        for x in range(x1 - 1, x2 + 1):
-            for y in range(y2 - 1, y1 + 1):
-                pix[x, y] = (255, 255, 255)
-
         image = cv.rectangle(image, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+    image2 = cv.rectangle(image2, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
 cv.imwrite("tes_detected_boxes.png",image)
-cv.imshow("Boxes", image)
-pil_img.show()
+cv.imwrite("tes_all_detected_boxes.png",image2)

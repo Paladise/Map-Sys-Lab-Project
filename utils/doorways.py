3e3484dc18ee862499f6ccea8472395b03b75d8b
andreibasto <andbas2015@gmail.com>
2023-03-06 08:45:23 -0500

    Add doorway detection pseudocode

--- utils/doorways.py
+++ utils/doorways.py
@@ -1,19 +1,22 @@
-import cv2 as cv
-import numpy as np
+"""
+Save boxes of identified rooms (remove ones that bound nothing)
 
-inputImage = cv.imread("rail.webp")
-inputImageGray = cv.cvtColor(inputImage, cv.COLOR_BGR2GRAY)
-edges = cv.Canny(inputImageGray,150,200,apertureSize = 3)
-minLineLength = 50
-maxLineGap = 5
-lines = cv.HoughLinesP(edges,cv.HOUGH_PROBABILISTIC, np.pi/180, 30, minLineLength,maxLineGap)
-for x in range(0, len(lines)):
-    for x1,y1,x2,y2 in lines[x]:
-        #cv.line(inputImage,(x1,y1),(x2,y2),(0,128,0),2, cv.LINE_AA)
-        pts = np.array([[x1, y1 ], [x2 , y2]], np.int32)
-        cv.polylines(inputImage, [pts], True, (0,255,0))
+For every room name
 
-font = cv.FONT_HERSHEY_SIMPLEX
-cv.putText(inputImage,"Tracks Detected", (500, 250), font, 0.5, 255)
-cv.imwrite("doorway_result.png", inputImage)
-cv.imwrite("edges.png", edges)
+    Expand out in all 4 cardinal directions until hits a wall
+
+        - Use to add padding for every room, but also use it as 
+        rudimentary way of getting doorways
+    
+For every room name
+    For every direction
+
+        Search in same direction along wall until comes along blank pixel and continue expanding
+        (stop if it hits another wall)
+
+        Check if made line intersects any (expanded) room boxes
+
+        If it doesn't intersect, assume it's a hallway
+        
+            Create doorway by removing pixels that are walls
+"""

305a47b38c2990b5ce674cb2aada163bd5487ab6
andreibasto <andbas2015@gmail.com>
2023-03-24 09:06:20 -0400

    Fix previous commit

--- utils/search.py
+++ utils/search.py
@@ -7,9 +7,9 @@ try:
 except:
     from drawing import remove_box
 
-THRESHOLD = .65 # Threshold needed to identify something as a symbol
+THRESHOLD = .6 # Threshold needed to identify something as a symbol
 
-def find_symbols(pil_image, pos_symbols, directory, symbol_files):
+def find_symbols(pil_image, pos_symbols, directory, symbol_files, debugging = False):
     """
     Find any remaining symbols given a blank image    
     """
@@ -26,12 +26,28 @@ def find_symbols(pil_image, pos_symbols, directory, symbol_files):
         res = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
     
         loc = np.where(res >= THRESHOLD)
-        for pt in zip(*loc[::-1]):  # Switch columns and rows            
+        pos_points = [pt for pt in zip(*loc[::-1])]
+        for pt in pos_points:  # Switch columns and rows      
             x, y = pt[0], pt[1]
-            if all((abs(x - x1) + abs(y - y1)) > 100 for x1, y1 in symbols[symbol]): # Add padding to prevent duplicates
-                remove_box(pixels, x, x + w, y, y + h)
-#                 cv.rectangle(image, pt, (x + w, y + h), (0, 0, 255), 2)
-                symbols[symbol].append((x + w//2, y + h//2)) # Append midpoint
+            
+            amount = 0
+            for x1, y1 in pos_points:
+                dist = ((x-x1)**2 + (y-y1)**2)**0.5
+                if dist < 75 and dist > 25:
+                    amount += 1
+            
+            if amount <= 1: # Ignore all ones that are close to other symbols
+                if all((abs(x - x1) + abs(y - y1)) > 100 for x1, y1 in symbols[symbol]): # Add padding to prevent duplicates
+                    remove_box(pixels, x, x + w, y, y + h)
+                    if debugging:
+                        if symbol in "stairs":
+                            cv.rectangle(image, pt, (x + w, y + h), (0, 0, 255), 1)
+                        else:
+                            cv.rectangle(image, pt, (x + w, y + h), (0, 255, 0), 1)
+                    symbols[symbol].append((x + w//2, y + h//2)) # Append midpoint
+    
+    if debugging:
+        cv.imwrite("debug_results/search_result_rect.png", image)
     
     return symbols, pil_image
 
@@ -53,7 +69,7 @@ def integrate_detected(rooms, found_symbols, stair_coords):
 
     
 if __name__ == "__main__":
-    symbols, image = find_symbols(Image.open("debug_images/blank2.png"), ["door", "stairs"], [])
+    symbols, image = find_symbols(Image.open("debug_images/blank2.png"), ["door", "stairs"], "debug_images/", {"door.png": "door", "stairs.png": "stairs"}, True)
     
     image.save("debug_results/search_result.png")
     
